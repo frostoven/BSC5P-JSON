484c1ae2e1880ad52053d89e47631a5f4d5e8d0b
aggregate1166877 <noreply@frostoven.com>
2023-09-30 02:00:46 +1300

    Improve cache error checker.

--- check_broken_cache.sh
+++ check_broken_cache.sh
@@ -18,5 +18,18 @@
 
 cd simbad.u-strasbg.fr_cache
 grep -L 'C\.D\.S' * | while read l; do cat "$l"; done
+du -sh ./* | sort -hr | tac | awk '{print $1 " " $2}' | grep '^0' | while read l; do
+  echo "Found zero-byte file: $l";
+done
 
+# All SIMBAD files end with a line of equal signs. We can use this as a missing
+# EOF check.
+echo 'Looking for missing end-of-file information...'
+ls | while read l; do
+  count="$(grep -c '===============================================================================' "./$l")"
+  if [ "$count" = '0' ]; then
+    echo "File $l appears to be incomplete..."
+  fi
+done
 
+echo 'All files checked.'
